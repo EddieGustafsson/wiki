aee94a293721a2f95a249eab2b5fb8f86498eac9
Viktor Karlsson Ferm <viktor.karlssonferm@elev.ga.ntig.se>
2019-11-20 09:19:20 +0100

    Lade till ett form i formuläret med bootstrap

--- dev/wiki.php
+++ dev/wiki.php
@@ -8,7 +8,29 @@
     </head>
 
     <body>
-        
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h3>Skapa Wiki</h3>
+                    <form action="10.130.216.101/TP/api.php" method="POST">
+                        <input type="hidden" name="nyckel" value="iRxOUsizwhoXddB4">
+                        <input type="hidden" name="tjanst" value="wiki">
+                        <input type="hidden" name="typ" value="function">
+                        <input type="hidden" name="handling" value="skapa">
+                        <input type="hidden" name="funktion" value="skapaWiki">
+                        <input type="hidden" name="anvandarId" value="4">
+                        <input type="text" name="titel" placeholder="Skriv en titel">
+                        <input type="submit" value="Skapa Wiki">
+                    </form>
+                </div>
+                <div class="col">
+                    <p>Paragraf Ryttare</p>
+                </div>
+                <div class="col">
+                    <p>Paragraf Ryttare</p>
+                </div>
+            </div>
+        </div>
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
