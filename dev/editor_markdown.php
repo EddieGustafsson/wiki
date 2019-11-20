d573cfbc874b08264a275cbe3b3ff8c9b3f7c783
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-11-20 08:58:18 +0100

    Skapade en markdown editor.

---
+++ dev/editor_markdown.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Editor Markdown</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/editor.md@1.5.0/css/editormd.min.css">
+    </head>
+    <body>
+
+        <div id="editor">
+            <!-- Tips: Editor.md can auto append a `<textarea>` tag -->
+            <textarea style="display:none;">### Hello Editor.md !</textarea>
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/editor.md@1.5.0/editormd.min.js"></script>
+        <script type="text/javascript">
+            $(function() {
+                var editor = editormd("editor", {
+                    width: "900px",
+                    height: "800px",
+                    // markdown: "xxxx",     // dynamic set Markdown text
+                    path : "../assets/js/lib/"  // Autoload modules mode, codemirror, marked... dependents libs path
+                });
+            });
+        </script>
+    </body>
+</html>
