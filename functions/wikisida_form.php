6fdd916d1f275bf880c6c223be82e0faa5a0e0b9
Alvar Ljungfjär <ljungfjaralvar@gmail.com>
2019-11-21 08:44:43 +0100

    kan nu skapa wikisidor som läggs in på wiki 6

--- functions/wikisida_form.php
+++ functions/wikisida_form.php
@@ -8,24 +8,27 @@
     </head>
     <body>
         <div id="wikisida-form" class="wikisida-form">
-            <h4>Skapa wikisida</h4>
 
-            <form action="http://10.130.216.101/TP/api.php" method="POST">
-            <input type='hidden' name='nyckel' value='iRxOUsizwhoXddb4'/>
-            <input type='hidden' name='tjanst' value='wiki'/>
-            <input type='hidden' name='typ' value='function'/>
-            <input type='hidden' name='handling' value='skapa'/>
-            <input type='hidden' name='funktion' value='skapaWikiUppdatering'/>
+            <div id="skapa-wikisida" class="skapa-wikisida">
+                <h4>Skapa wikisida</h4>
 
-            Wiki id: <input type="text" name="wikiId"><br>
-            Sid id: <input type="text" name="sidId"><br>
-            Bidragare: <input type="text" name="bidragsgivare"><br>
-            Titel: <input type="text" name="titel"><br>
-            Innehåll: <input type="text" name="innehall"><br>
+                <form action="http://10.130.216.101/TP/api.php" method="POST">
+                    <input type='hidden' name='nyckel' value='JIOAJWWNPA259FB2'/>
+                    <input type='hidden' name='tjanst' value='wiki'/> 
+                    <input type='hidden' name='typ' value='function'/>
+                    <input type='hidden' name='handling' value='skapa'/>
+                    <input type='hidden' name='funktion' value='skapaWikiUppdatering'/>
+                    <input type="hidden" name="wikiId" value="6">
 
-            <input type="submit" value="Skapa wikisida">
-            </form>
+                    Sid id: <input type="text" name="sidId"><br>
+                    Bidragare: <input type="text" name="bidragsgivare"><br>
+                    Titel: <input type="text" name="titel"><br>
+                    Innehåll: <input type="text" name="innehall"><br>
+
+                    <input type="submit" value="Skapa wikisida">
+                </form>
         </div>
+
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
