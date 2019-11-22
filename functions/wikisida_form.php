f83574a4178811df7cbb0f55391e6c10bc6f8464
Alvar Ljungfjär <ljungfjaralvar@gmail.com>
2019-11-22 07:20:24 +0100

    skiftar över till ubuntu

--- functions/wikisida_form.php
+++ functions/wikisida_form.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <title></title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/editor.md@1.5.0/css/editormd.min.css">
     </head>
     <body>
         <div id="wikisida-form" class="wikisida-form">
@@ -23,7 +23,11 @@
                     Sid id: <input type="text" name="sidId"><br>
                     Bidragare: <input type="text" name="bidragsgivare"><br>
                     Titel: <input type="text" name="titel"><br>
-                    Innehåll: <input type="text" name="innehall"><br>
+                    <p>Innehåll:</p>
+                    <div id="editor">
+                        <!-- Tips: Editor.md can auto append a `<textarea>` tag -->
+                         <textarea name="innehall" style="display:none;"></textarea>
+                    </div>
 
                     <input type="submit" value="Skapa wikisida">
                 </form>
@@ -33,6 +37,46 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/editor.md@1.5.0/editormd.min.js"></script>
+        <script type="text/javascript">
+        var editor;
+            $(function() {
+                    editor = editormd("editor", {
+                    width: "900px",
+                    height: "800px",
+
+                    theme : "dark",
+                    previewTheme : "dark",
+                    editorTheme : "pastel-on-dark",
+
+                    codeFold : true,
+                    htmlDecode : "style,script,iframe|on*",
+                    emoji: true,
+                    taskList: true,
+                    tocm: true,
+                    tex: true,
+                    flowChart: true,
+                    sequenceDiagram: true,
+                    autoLoadModules: true,
+                    lineNumbers: true, // Display editor line numbers
+
+                    imageUpload: true,
+                    imageFormats: ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
+                    imageUploadURL: "./php/upload.php",
+                    
+                    path : "../assets/js/lib/"  // Autoload modules mode, codemirror, marked... dependents libs path
+                });
+            });
+
+            for(let i = 0; i < editor.length; i++)
+            {
+                if(editor.indexOf(i) == '\"' || editor.indexOf(i) == "\'"){
+                editor.indexOf(i) = "2_%1"
+                var_dump(editor.indexOf(i));
+            }
+
+        </script>
     </body>
 </html>
 
