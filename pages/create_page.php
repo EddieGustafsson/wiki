8f95609907b2448bff5da4002e5b4d8a0f72a80c
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-11-26 10:29:22 +0100

    Lagt till en sidtitel.

--- pages/create_page.php
+++ pages/create_page.php
@@ -1,5 +1,5 @@
 <?php
-$page_title = 'Skapa en nya artikel';
+$page_title = 'Skapa en ny artikel';
 include '../includes/head.php';
 ?>
         <main role="main" class="flex-shrink-0">
@@ -13,35 +13,45 @@ include '../includes/head.php';
             </div>
 
             <div class="container">
-                <div class="card shadow-lg">
-                    <div class="card-header">
-                        <div class="row">
-                            <div class="col-8">
-                                <div class="input-group input-group-lg">
-                                    <input type="text" class="form-control" aria-label="Sizing example input" placeholder="Titel" aria-describedby="inputGroup-sizing-lg">
+                <form method="POST" action="">
+                    <input type="hidden" name="page_id" value="0">
+                    <input type="hidden" name="user_id" value="0">
+                    <div class="card shadow-lg">
+                        <div class="card-header">
+                            <div class="row">
+                                <div class="col-8">
+                                    <div class="input-group input-group-lg">
+                                        <input name="title" type="text" class="form-control" aria-label="Sizing example input" placeholder="Titel" aria-describedby="inputGroup-sizing-lg">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-4">
-                                <p>Inställningar:</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col">
-                                <div id="editor">
-                                    <textarea style="display:none;"></textarea>
-                                </div>
-                                <div>
-                                    <input type="text" class="form-control" aria-label="Sizing example input" placeholder="Skriv ett litet meddelande här som förklarar denna förändring. (Valfri)" aria-describedby="inputGroup-sizing-default">
-                                </div>
-                                <div style="margin-top:10px;">
-                                    <button type="button" class="btn btn-outline-success">Skapa artikel</button>
+                                <div class="col-4">
+                                    <p style="margin-top:1px;margin-bottom:-2px;">Inställningar:</p>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                                        <label class="form-check-label" for="defaultCheck1">
+                                            Lås artikel
+                                        </label>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <div id="editor">
+                                        <textarea name="source" style="display:none;"></textarea>
+                                    </div>
+                                    <div>
+                                        <input type="text" class="form-control" aria-label="Sizing example input" placeholder="Skriv ett litet meddelande här som förklarar denna förändring. (Valfri)" aria-describedby="inputGroup-sizing-default">
+                                    </div>
+                                    <div style="margin-top:10px;">
+                                        <button type="submit" class="btn btn-outline-success">Skapa artikel</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
         </main>
 
