b006aeddc7a08ad3d8a8fda9cfc9a5418a7cfe90
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-11-27 13:44:50 +0100

    Ändrade länkar i navbar.

--- includes/head.php
+++ includes/head.php
@@ -32,7 +32,7 @@
                 </div>
             </nav>
             <nav class="navbar navbar-expand-lg navbar-dark bg-danger shadow" style="background-color: #ce1022 !important;">
-                <a class="navbar-brand" href="#">Marvel Wiki</a>
+                <a class="navbar-brand" href="home">Marvel Wiki</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -40,7 +40,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#"><i class="fa fa-home"></i> Hem <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="home"><i class="fa fa-home"></i> Hem <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -69,7 +69,7 @@
                         <i class="fa fa-compass"></i> Utforska
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Slumpartikel</a>
+                            <a class="dropdown-item" href="/Wiki/_random">Slumpartikel</a>
                             <a class="dropdown-item" href="#">Senaste ändringarna</a>
                         </div>
                     </li>
