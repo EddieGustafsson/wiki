dabfc9d34cf6a37e305e9c97748b84085eebbdcf
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-11-28 10:45:28 +0100

    Ändrade modal.

--- includes/modals.php
+++ includes/modals.php
@@ -1,6 +1,6 @@
 <div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="login" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="login">Logga in till Marvel Wiki</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -23,6 +23,6 @@
                         <button type="button" class="btn btn-danger btn-lg">Logga in</button>
                     </div>
                 </div>
-                </div>
             </div>
-        </div>
+    </div>
+</div>
