1509c81dd508810cdf50f9f91fb5d481e28a2a0f
Viktor Karlsson Ferm <viktor.karlssonferm@elev.ga.ntig.se>
2019-12-04 15:21:03 +0100

    Flyttade in all kod under for loopen in i for loopen

--- includes/footer.php
+++ includes/footer.php
@@ -63,23 +63,23 @@
                         //console.log(lank.getAttribute("data-original-title"));
                         //console.log(i);
 
+                        let desc = jsonData.sidor[3].innehall; //0 bör ändras till page_id
+
+                        let maxLength = 200; // maximum number of characters to extract
+
+                        //trim the string to the maximum length
+                        let trimmedString = desc.substr(0, maxLength);
+
+                        //re-trim if we are in the middle of a word
+                        trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")));
+                        trimmedString = trimmedString.replace(/#|>/g,'');
+                        //trimmedString = trimmedString.replace(/ \"[\s\S]*?\"/g, ''); //Trims Citations
+
+                        var popover = $('[data-toggle="popover"]');
+
+                        popover.attr("data-content",trimmedString + "...").data('popover');
+
                     }
-
-                    let desc = jsonData.sidor[3].innehall; //0 bör ändras till page_id
-
-                    let maxLength = 200; // maximum number of characters to extract
-
-                    //trim the string to the maximum length
-                    let trimmedString = desc.substr(0, maxLength);
-
-                    //re-trim if we are in the middle of a word
-                    trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")));
-                    trimmedString = trimmedString.replace(/#|>/g,'');
-                    //trimmedString = trimmedString.replace(/ \"[\s\S]*?\"/g, ''); //Trims Citations
-
-                    var popover = $('[data-toggle="popover"]');
-
-                    popover.attr("data-content",trimmedString + "...").data('popover');
                     
                 }
 
