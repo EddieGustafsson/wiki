92a7061d94266e838322bc4fd79d969da4191c8c
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-12-12 13:45:03 +0100

    Lagt till en alert som visar att användaren är på en tidigare version av en artikel.

--- pages/show_history_page.php
+++ pages/show_history_page.php
@@ -5,11 +5,12 @@ include "../functions/get_history.php";
 
 if(isset($_GET['version']) && isset($_GET['id'])){
     $page_id = $_GET['id'];
-    $page_title = $_GET['title'];
     $version_id = $_GET['version'];
     $array = getHistory($page_id);
     $index = $_GET['index'];
     $parent_branch = $_GET['branch_title'];
+
+    $page_title = '#'.$version_id.' '.$array[$index]['titel'].'';
 }
 
 include '../includes/head.php';
@@ -25,7 +26,7 @@ include '../includes/head.php';
                             <li class="breadcrumb-item"><a href="/Wiki">Hem</a></li>
                             <li class="breadcrumb-item"><a href=/Wiki/'. $parent_branch.'>'.$parent_branch.'</a></li>
                             <li class="breadcrumb-item"><a href=/Wiki/'. $parent_branch.'/_history>Historik</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">'.$array[$index]['titel'].'</li>
+                            <li class="breadcrumb-item active" aria-current="page">#'.$version_id.' '.$array[$index]['titel'].'</li>
                             ';
                         
                         ?>
@@ -42,15 +43,22 @@ include '../includes/head.php';
                                     <li class="nav-item">
                                         <a class="nav-link active">Artikel</a>
                                     </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="<?php echo ''.$host.'/'.$parent_branch.'/_history';?>">Gå tillbaka till historik</a>
+                                    </li>
                                 </ul>
                                 <br>
                                 <h1>
                                 <?php
-                                    echo $array[$index]['titel']; 
+                                    echo $array[$index]['titel'];
+                                    echo ' (#'.$version_id.')'; 
                                 ?></h1>
                                 <p><i>Senast ändrad: <?php echo $array[$index]['datum']; ?></i></p>
                             </div>
-                        
+                            <div class="col-4 alert alert-warning">
+                                <h5><i class="fas fa-exclamation-triangle"></i> Förhandsgranskning av artikel</h5><br>
+                                <p>Detta är en tidigare version av artikeln <em><?php echo $array[$index]['titel']?></em>, som publicerades <em><?php echo $array[$index]['datum']; ?></em>.</p>
+                            </div>
                         </div>
                     </div>
                     <div class="card-body">
@@ -64,9 +72,25 @@ include '../includes/head.php';
                                 </div>
                             </div>
                             <div class="col-4" style="border-left: 1px solid rgba(0,0,0,.125);text-align:left;">
-                                <div class="sticky-top" style="top: 100px; z-index: 1;">
-                                    <h3>Innehållsförteckning</h3>
-                                    <div id="custom-toc-container"></div>
+                            <div id="accordion">
+                                    <div class="card">
+                                        <div class="card-header" id="headingOne">
+                                            <h5 class="mb-0">
+                                                <button class="btn" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                    <h5>Innehållsförteckning</h5>
+                                                </button>
+                                                <div class="float-right" data-toggle="collapse" data-target="#collapseOne" style="margin-top: 3%;">
+                                                    <i class="fa" aria-hidden="true"></i>
+                                                </div>
+                                            </h5>
+                                        </div>
+
+                                        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                                            <div class="card-body">
+                                                <div id="custom-toc-container"></div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
