49bd23bfdd92b204309061e0c6e9f4f7ddda7bec
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-12-12 13:43:39 +0100

    Ändrade script-taggar i head.

--- includes/head.php
+++ includes/head.php
@@ -40,6 +40,7 @@ $link = basename($_SERVER["REQUEST_URI"]);
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="<?php echo $host;?>/assets/js/search_auto_complete.js"></script>
 
     </head>
     
@@ -80,7 +81,7 @@ $link = basename($_SERVER["REQUEST_URI"]);
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="<?php echo $host;?>/home"><i class="fa fa-home"></i> Hem <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="<?php echo $host;?>/home"><i class="fa fa-home"></i> Hem <span class="sr-only">(current)<?php if($title == "Home"){echo "active";}?></span></a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -116,7 +117,7 @@ $link = basename($_SERVER["REQUEST_URI"]);
                     </li>
                     </ul>
                     <form class="form-inline my-2 my-lg-0" action="<?php echo $host;?>/_search" method="GET">
-                        <input name="query" class="form-control mr-sm-2" type="search" placeholder="Sök i Marvel Wiki" aria-label="Search">
+                        <input name="query" id="query" class="form-control typeahead mr-sm-2" type="text" ata-provide="typeahead" autocomplete="off" placeholder="Sök i Marvel Wiki" aria-label="Search">
                         <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Sök</button>
                     </form>
                 </div>
