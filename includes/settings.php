de947d753422d9916a599f2b8b19c0cef3fdb105
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-12-17 14:12:01 +0100

    Ändrade api-nycklar och ID

--- includes/settings.php
+++ includes/settings.php
@@ -1,5 +1,5 @@
 <?php
-$api = "nx06YHDvPELOArYg";
+$api = "x9qXnagSOsS6sHun";
 $host = "/Wiki";
-$wiki_id = "8";
+$wiki_id = "9";
 $stylesArr = array('main', 'darkmode', 'zebra', 'color-blind', 'polka', 'comicmode');
