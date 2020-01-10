929f342ff0d5a5e315cae1bed1330d63829576a5
Viktor Karlsson Ferm <viktor.karlssonferm@elev.ga.ntig.se>
2020-01-10 10:47:52 +0100

    Skapade en ny funktion för att aktivera inaktiva användare

---
+++ functions/activate_user.php
@@ -0,0 +1,33 @@
+<?php
+
+include "../includes/settings.php";
+
+if(isset($_POST['user_id'])){
+
+    $function = 'aktiveraKonto';
+    $user_id = $_POST['user_id'];
+
+    // Create map with request parameters
+    $params = array ('funktion' => $function, 'id' => $user_id);
+
+    $query = http_build_query($params);
+
+    // Create Http context details
+    $context_data = array (
+        'method' => 'POST',
+        'header' => "Connection: close\r\n".
+                    "Content-Type: application/x-www-form-urlencoded\r\n".
+                    "Content-Length: ".strlen($query)."\r\n",
+        'content'=> $query );
+
+    $context = stream_context_create(array('http' => $context_data));
+
+    $result = file_get_contents('http://10.130.216.101/TP/Admin/funktioner/konto.php', false, $context);
+
+    $array = json_decode($result, true);
+
+    header("location: " . $host . "/_admin");
+
+}
+
+?>
