a05b561e5960f27b1357faee3394dd2b967b7269
Viktor Karlsson Ferm <viktor.karlssonferm@elev.ga.ntig.se>
2020-01-15 10:34:29 +0100

    Lade till krav på roll i advanced_setting.php

--- functions/advanced_settings.php
+++ functions/advanced_settings.php
@@ -1,15 +1,20 @@
 <?php 
 
+session_start();
 include '../includes/settings.php';
 
-if(isset($_POST['debug'])) {
-    if($_POST['debug'] == true){
-        setcookie("debug", 'on', time()+(60*60*24*30), '/');
-        header('Location: ' . $host . '/_settings?status=settings&reason=DebugEnabled');
-    } 
-} else {
-    setcookie("debug", 'off', time()+(60*60*24*30), '/');
-    header('Location: ' . $host . '/_settings?status=settings&reason=DebugDisabled');
+if(isset($_SESSION['role'])){
+
+    if(isset($_POST['debug'])) {
+        if($_POST['debug'] == true){
+            setcookie("debug", 'on', time()+(60*60*24*30), '/');
+            header('Location: ' . $host . '/_settings?status=settings&reason=DebugEnabled');
+        } 
+    } else {
+        setcookie("debug", 'off', time()+(60*60*24*30), '/');
+        header('Location: ' . $host . '/_settings?status=settings&reason=DebugDisabled');
+    }
+
 }
 
 exit();
